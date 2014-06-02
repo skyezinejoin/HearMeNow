b5ec059251d6ba10afa4c22fe86fb77337034f2e
Lau Philip <philiplau@Philips-MacBook-Pro.local>
2014-06-02 16:16:49 -0700

    Initial Commit

---
+++ HearMeNow/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HearMeNow
+//
+//  Created by Lau Philip on 6/2/14.
+//  Copyright (c) 2014 Ubersoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ HearMeNowTests/HearMeNowTests.m
@@ -0,0 +1,34 @@
+//
+//  HearMeNowTests.m
+//  HearMeNowTests
+//
+//  Created by Lau Philip on 6/2/14.
+//  Copyright (c) 2014 Ubersoft. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HearMeNowTests : XCTestCase
+
+@end
+
+@implementation HearMeNowTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
